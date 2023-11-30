6651ee06f4228d7a3c5857ff7325da7023784283
Mfon <uwahsheedo@gmail.com>
2023-11-30 14:25:41 +0100

    commit

---
+++ lib/ui/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:prestmit/ui/utils/app_color.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primary,
+      body: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
+  }
+}
---
+++ lib/ui/utils/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor();
+
+  static const Color primary = Color(0XFFFFCE23);
+  static const Color skyBlue = Color(0XFF3DDEED);
+}
